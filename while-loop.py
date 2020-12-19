874b881927eba961b17e032c38447b23bb059ff6
purwokang <purwo.com@gmail.com>
2020-12-19 22:46:08 +0700

    loop dengan while

---
+++ while-loop.py
@@ -0,0 +1,20 @@
+# Loop
+# - While
+
+# print("yeaaayyyy saya akan menikah!")
+# print("yeaaayyyy saya akan menikah!")
+# print("yeaaayyyy saya akan menikah!")
+# print("yeaaayyyy saya akan menikah!")
+# print("yeaaayyyy saya akan menikah!")
+# print("yeaaayyyy saya akan menikah!")
+
+
+# bagaimana agar tidak perlu mengetik ulang:
+
+count = 0
+
+while count < 5:
+    print("yeaaayyyy saya akan menikah!")  # print sebanyak 5 kali.
+    count = count+1
+else:  # pemakian else sifatnya optional
+    print("huft akhirnya berhenti juga, nilai count sekarang adalah ", count)
