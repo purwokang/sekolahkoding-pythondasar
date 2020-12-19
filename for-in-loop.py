6913078865f06f1155212481fb68060eb965fb8a
purwokang <purwo.com@gmail.com>
2020-12-19 22:54:57 +0700

    mencetak / mengeluarkan isi variable dengan for in

---
+++ for-in-loop.py
@@ -0,0 +1,11 @@
+# Loop
+# - While
+# - For In
+
+# orang = ['hilman', 'boss', 'komeng']
+# for nama in orang:
+#     print('nama namanya adalah: ', nama)
+
+text = 'python'
+for huruf in text:
+    print("cekidot: ", huruf)
